000d0b43e913f08305c6a2a73bfaffc0d6662b2b
goldIsApple <kes98202@naver.com>
2024-01-16 20:44:27 +0900

    수열

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,53 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 public class Main {
 
-	public static void main(String[] args) {
-		System.out.println("Hello world!");
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int size = Integer.parseInt(br.readLine());
+		int[] numbers = Arrays.stream(br.readLine().split(" "))
+			.mapToInt(Integer::parseInt)
+			.toArray();
+
+		int max = 1;
+		for (int i = 0; i < size; i++) {
+			int count = 1;
+			int order = 0;// 0 초기화 1:left 2:right
+			if(size-max+1==i ){
+				System.out.println(max);
+				return;
+			}
+			for (int j = i; j < size-1; j++) {
+				int leftNumber = numbers[j];
+				int rightNumber = numbers[j+1];
+
+				if(leftNumber==rightNumber){
+					count++;
+				}else if(leftNumber<rightNumber){
+					if(order==1){
+						break;
+					}
+					order=2;
+					count++;
+				}else if(leftNumber>rightNumber){
+					if(order==2){
+						break;
+					}
+					order=1;
+					count++;
+				}
+
+			}
+			if(count>max){
+				max=count;
+			}
+		}
+		System.out.println(max);
+
 	}
 }
