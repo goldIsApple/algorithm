ac0d1322fbb4b161ac60e6ce3a4542f0627615b8
goldIsApple <kes98202@naver.com>
2024-01-27 20:35:15 +0900

    two pointers algorithm

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,12 +3,34 @@ package org.example;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int size = Integer.parseInt(br.readLine());
-		String[] str = new String[size];
+		int[] a = {1,3,5};
+		int[] b = {2, 3, 6, 7, 9};
+		int ac = 0,bc = 0;
+		List<Integer> data = new ArrayList<>();
+		while(ac<a.length && bc<b.length){
+			if(a[ac] <= b[bc]){
+				data.add(a[ac]);
+				ac++;
+			}else if(a[ac] > b[bc]){
+				data.add(b[bc]);
+				bc++;
+			}
+		}
+		while(ac<a.length){
+			data.add(a[ac]);
+			ac++;
+		}
+		while(bc<b.length){
+			data.add(b[bc]);
+			bc++;
+		}
+		System.out.println(data);
 	}
 }
